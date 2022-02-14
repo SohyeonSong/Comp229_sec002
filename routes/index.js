c1809b12e39c6e6d3f636bcf20a52cea1dbe43f6
SohyeonSong <thdth1113@gmail.com>
2022-02-14 16:31:58 -0500

    develop

--- routes/index.js
+++ routes/index.js
@@ -4,20 +4,42 @@ StudentID : 301145311
 Date : 2022-02-05
 */
 
+let express = require('express');
+let router = express.Router();
 
-var express = require('express');
-var router = express.Router();
-let indexcontroller = require('../controller/index');
+let indexController = require('../controllers/index');
 
 /* GET home page. */
-router.get('/', indexcontroller.home);
+router.get('/', indexController.displayHomePage);
+
+/* GET home page. */
+router.get('/home', indexController.displayHomePage);
+
+/* GET about page. */
+router.get('/about', indexController.displayAboutPage);
 
 /* GET Projects page. */
-router.get('/projects',indexcontroller.projects );
+router.get('/projects', indexController.displayProjectsPage);
 
-/* GET About page. */
-router.get('/about', function(req, res, next) {
-  res.render('index', { title: 'About' });
-});
+/* GET Services page. */
+router.get('/services', indexController.displayServicesPage);
+
+/* GET Contact page. */
+router.get('/contact', indexController.displayContactPage);
+
+/* GET Route for the displaying Login Page */
+router.get('/login', indexController.displayLoginPage);
+
+/* GET Route for the processing Login Page */ 
+router.post('/login', indexController.processLoginPage);  
+
+/* GET Route for the displaying Register Page */
+router.get('/register', indexController.displayRegisterPage);
+
+/* GET Route for the processing Register Page */ 
+router.post('/register', indexController.processRegisterPage);  
+
+/* GET to perform UserLogout */
+router.get('/logout', indexController.performLogout);
 
 module.exports = router;
